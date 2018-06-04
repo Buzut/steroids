33cbec6469fa10a848c5f3116f3b3c1d82ed1898
Buzut <blog@buzut.fr>
2018-06-04 14:48:57 +0200

    GDPR compliance, fuck GA, use Matomo if needed

--- header.php
+++ header.php
@@ -11,15 +11,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 		<?php wp_head(); ?>
-
-        <script async>
-    		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-    		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-    		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-    		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-    		ga('send', 'pageview');
-		</script>
 	</head>
 
 	<body <?php body_class(); ?>>
