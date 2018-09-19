c77b39ef5a199afa7997146421b5271fe4183542
Buzut <blog@buzut.fr>
2018-09-19 10:39:49 +0200

    remove cruft in http header and html head

--- functions.php
+++ functions.php
@@ -280,6 +280,9 @@ remove_action('wp_head', 'wp_generator'); // Display the XHTML generator that is
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 remove_action('wp_head', 'rel_canonical');
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action('wp_head', 'rest_output_link_wp_head', 10); // Disable REST API link tag
+remove_action('wp_head', 'wp_oembed_add_discovery_links', 10); // Disable oEmbed Discovery Links
+remove_action('template_redirect', 'rest_output_link_header', 11, 0); // Disable REST API link in HTTP headers
 
 // Remove emojis
 remove_action('wp_head', 'print_emoji_detection_script', 7);
