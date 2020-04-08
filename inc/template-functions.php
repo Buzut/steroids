e1a2aec50442ac9e0e0efb38a44f566f26843e76
Buzut <blog@buzut.fr>
2020-04-08 15:49:03 +0200

    💄 (pagination) clean implementation

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -8,11 +8,9 @@ defined('ABSPATH') || exit;
 // Pagination for paged posts, Page 1, Page 2, Page 3, with Next and Previous Links, No plugin
 function steroids_pagination() {
     global $wp_query;
-    $big = 999999999;
 
     echo paginate_links([
-        'base' => str_replace($big, '%#%', get_pagenum_link($big)),
-        'format' => '?paged=%#%',
+        'format' => 'page/%#%/',
         'current' => max(1, get_query_var('paged')),
         'total' => $wp_query->max_num_pages
     ]);
