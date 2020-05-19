47f2556c0f7479bfe0331d0610c0209358db222b
Buzut <blog@buzut.fr>
2020-05-19 09:40:30 +0200

    🚧 simplify page code

--- page.php
+++ page.php
@@ -1,21 +1,10 @@
 <?php defined('ABSPATH') || exit; ?>
 <?php get_header(); ?>
 
-<main>
+<main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <h1><?php the_title(); ?></h1>
-
-    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <?php the_content(); ?>
-            <?php comments_template('', true); // Remove if you don't want comments ?>
-        </article>
-    <?php endwhile; ?>
-
-    <?php else: ?>
-        <article>
-            <h2><?php _e('Sorry, nothing to display.', 'steroids'); ?></h2>
-        </article>
-    <?php endif; ?>
+    <?php the_content(); ?>
+    <?php comments_template('', true); // Remove if you don't want comments ?>
 </main>
 
 <?php get_sidebar(); ?>
