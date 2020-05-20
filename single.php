7520b932219612a23a9e9fc979ba4f583b1f277d
Buzut <blog@buzut.fr>
2020-05-20 12:37:55 +0200

    🚧 (templates) remove if & while cruft

--- single.php
+++ single.php
@@ -3,41 +3,33 @@ defined('ABSPATH') || exit;
 get_header();
 ?>
 <main>
-    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <!-- post thumbnail -->
-            <?php if (has_post_thumbnail()) : // Check if Thumbnail exists ?>
-                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                    <?php the_post_thumbnail(); // Fullsize image for the single post ?>
-                </a>
-            <?php endif; ?>
-            <!-- /post thumbnail -->
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <!-- post thumbnail -->
+        <?php if (has_post_thumbnail()) : // Check if Thumbnail exists ?>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                <?php the_post_thumbnail(); // Fullsize image for the single post ?>
+            </a>
+        <?php endif; ?>
+        <!-- /post thumbnail -->
 
-            <h1>
-                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-            </h1>
+        <h1>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+        </h1>
 
-            <!-- post details -->
-            <span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-            <span class="author"><?php _e('Published by', 'steroids'); ?> <?php the_author_posts_link(); ?></span>
-            <span class="comments"><?php if (comments_open(get_the_ID())) comments_popup_link(__('Leave your thoughts', 'steroids'), __('1 Comment', 'steroids'), __('% Comments', 'steroids')); ?></span>
-            <!-- /post details -->
+        <!-- post details -->
+        <span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+        <span class="author"><?php _e('Published by', 'steroids'); ?> <?php the_author_posts_link(); ?></span>
+        <span class="comments"><?php if (comments_open(get_the_ID())) comments_popup_link(__('Leave your thoughts', 'steroids'), __('1 Comment', 'steroids'), __('% Comments', 'steroids')); ?></span>
+        <!-- /post details -->
 
-            <?php the_content(); // Dynamic Content ?>
-            <?php the_tags(__('Tags: ', 'steroids'), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+        <?php the_content(); // Dynamic Content ?>
+        <?php the_tags(__('Tags: ', 'steroids'), ', ', '<br>'); // Separated by commas with a line break at the end ?>
 
-            <p><?php _e('Categorised in: ', 'steroids'); the_category(', '); // Separated by commas ?></p>
-            <p><?php _e('This post was written by ', 'steroids'); the_author(); ?></p>
+        <p><?php _e('Categorised in: ', 'steroids'); the_category(', '); // Separated by commas ?></p>
+        <p><?php _e('This post was written by ', 'steroids'); the_author(); ?></p>
 
-            <?php comments_template(); ?>
-        </article>
-    <?php endwhile; ?>
-
-    <?php else: ?>
-        <article>
-            <h1><?= __('Sorry, nothing to display.', 'steroids') ?></h1>
-        </article>
-    <?php endif; ?>
+        <?php comments_template(); ?>
+    </article>
 </main>
 
 <?php get_sidebar(); ?>
--- template-demo.php
+++ template-demo.php
@@ -7,18 +7,10 @@ get_header();
 <main>
     <h1><?php the_title(); ?></h1>
 
-    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <?php the_content(); ?>
-            <?php comments_template('', true); // Remove if you don't want comments ?>
-        </article>
-    <?php endwhile; ?>
-
-    <?php else: ?>
-        <article>
-            <h2><?php _e('Sorry, nothing to display.', 'steroids'); ?></h2>
-        </article>
-    <?php endif; ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php the_content(); ?>
+        <?php comments_template('', true); // Remove if you don't want comments ?>
+    </article>
 </main>
 
 <?php get_sidebar(); ?>
