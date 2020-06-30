ef62cc230a63c20c25bc5c66410cdb353e026d16
Buzut <blog@buzut.fr>
2020-06-30 10:29:10 +0200

    💄 replace _e by __ w/ short tags

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,5 +1,6 @@
 <?php defined('ABSPATH') || exit; ?>
+
 <form class="search" method="get" action="<?= home_url() ?>" role="search">
-    <input class="search-input" type="search" name="s" placeholder="<?php _e('To search, type and hit enter.', 'steroids'); ?>">
-    <button class="search-submit" type="submit"><?php _e('Search', 'steroids'); ?></button>
+    <input class="search-input" type="search" name="s" placeholder="<?= __('To search, type and hit enter.', 'steroids') ?>">
+    <button class="search-submit" type="submit"><?= __('Search', 'steroids') ?></button>
 </form>
