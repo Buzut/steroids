dd41d297eea6b2e00bd76e8d13059a68c3259889
Buzut <blog@buzut.fr>
2020-11-19 19:14:25 +0100

    💄 (tag) replace echo by short echo tag

--- tag.php
+++ tag.php
@@ -4,7 +4,7 @@ get_header();
 ?>
 <main>
     <h1>
-        <?php _e('Tag Archive: ', 'steroids'); echo single_tag_title('', false); ?>
+        <?= __('Tag Archive: ', 'steroids'); single_tag_title('', false); ?>
     </h1>
 
     <?php get_template_part('loop'); ?>
