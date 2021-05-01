148fc126cd1e0a36d300285be438e31d630388b4
Buzut <blog@buzut.fr>
2021-05-01 08:44:32 +0200

    fix script

--- install.sh
+++ install.sh
@@ -22,15 +22,15 @@ fi
 
 if [[ $9 == "" ]]
 then
-    mysql -u "$myuser" -e "CREATE DATABASE $1 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
+    mysql -u "$myuser" -e "CREATE DATABASE $1 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
 else
-    mysql -u "$myuser" -p"$9" -e "CREATE DATABASE $1 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
+    mysql -u "$myuser" -p"$9" -e "CREATE DATABASE $1 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
 fi
 
 mkdir wordpress && cd wordpress \
 && curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
 && php wp-cli.phar core download --locale=$locale \
-&& php wp-cli.phar config create --dbname=$1 --dbuser=root --dbpass="" --dbhost=127.0.0.1 --dbcharset=utf8mb4 --dbcollate=utf8mb4_unicode_ci \
+&& php wp-cli.phar config create --dbname=$1 --dbuser=root --dbpass="$9" --dbhost=127.0.0.1 --dbcharset=utf8mb4 --dbcollate=utf8mb4_unicode_ci \
 && php wp-cli.phar core install --title="$2" --url="$3" --admin_user="$4" --admin_password="$5" --admin_email=$6 \
 && php wp-cli.phar theme install https://github.com/Buzut/steroids/archive/master.zip --activate \
 && php wp-cli.phar theme uninstall `ls wp-content/themes/ | grep -v steroids | grep -v index.php | xargs` \
