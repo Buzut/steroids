eeb175a48dcae50535ffe3ce5d4c6404f58daee5
Buzut <blog@buzut.fr>
2021-06-03 10:01:32 +0200

    🌟 add Steroids namespace

--- public/themes/steroids/inc/templates.php
+++ public/themes/steroids/inc/templates.php
@@ -1,4 +1,6 @@
 <?php
+namespace Steroids;
+
 defined('ABSPATH') || exit;
 require dirname(__DIR__, 3) . '/vendor/autoload.php';
 
@@ -7,7 +9,7 @@ require dirname(__DIR__, 3) . '/vendor/autoload.php';
  */
 
 // Pagination for paged posts, Page 1, Page 2, Page 3, with Next and Previous Links, No plugin
-function steroids_pagination() {
+function print_pagination() {
     global $wp_query;
 
     echo paginate_links([
@@ -18,17 +20,17 @@ function steroids_pagination() {
 }
 
 // Custom View Article link to Post
-function steroids_view_article($more) {
+function print_view_article($more) {
     global $post;
     return '… <a class="view-article" href="' . get_permalink($post->ID) . '">' . __('View Article', 'steroids') . '</a>';
 }
-// add_filter('excerpt_more', 'steroids_view_article');
+// add_filter('excerpt_more', 'Steroids\print_view_article');
 
 // If posts (or anything) are saved in Md, this ƒn will convert it into HTML
 // To be used with get_the_content (for posts/pages) because the_content echoes instead of returning
 // Alse be carefull because the_content doesn't any sanitisation (you can apply them later on if required)
-function steroids_parse_markdown($content, $echo = false) {
-    $Parsedown = new Parsedown();
+function parse_markdown($content, $echo = false) {
+    $Parsedown = new \Parsedown();
 
     if ($echo) echo $Parsedown->text(wptexturize($content));
     return $Parsedown->text(wptexturize($content));
